a18a69833183d19fb8c2655632db3d59b3f7b70f
Ralph Jan Gallegos <ralphjangallegos@gmail.com>
2024-08-19 22:00:11 +0800

    added fetch applied job

--- api/headers.php
+++ api/headers.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json");
 
-// If this is an OPTIONS request, we exit early
+
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(204); // No content
     exit;
